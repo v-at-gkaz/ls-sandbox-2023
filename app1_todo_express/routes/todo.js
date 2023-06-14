e946d55de18a314edd4a826fddacb66aa918602d
Vladimir <v@gkaz.ru>
2023-06-14 20:27:20 +0300

    Improve code in app1_todo_express/routes/todo.js

--- app1_todo_express/routes/todo.js
+++ app1_todo_express/routes/todo.js
@@ -1,10 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { expressjwt: jwt } = require('express-jwt');
-
-
 const todoCtrl = require('../controllers/todo');
-
 const secret = process.env.JWT_SECRET;;
 
 const auth = jwt({
@@ -12,19 +9,10 @@ const auth = jwt({
   algorithms: ["HS256"]
 });
 
-/* GET todo listing */
 router.get('/', auth, todoCtrl.getAll);
-
-/* GET todo by id */
 router.get('/:id', auth, todoCtrl.getById);
-
-/* POST -- Create new todo */
 router.post('/', auth, todoCtrl.create);
-
-/* PATCH -- Update todo by id */
 router.patch('/:id', auth, todoCtrl.update);
-
-/* DELETE -- Delete todo by id */
 router.delete('/:id', auth, todoCtrl.delete);
 
 module.exports = router;
